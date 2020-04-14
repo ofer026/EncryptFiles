6d1bdcb0d8e9807a3cdabde35bc6a331fd44a529
Ofer <ofer026@gmail.com>
2020-04-15 00:03:08 +0300

    File encrypting works

--- Window/window.py
+++ Window/window.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
+import pyautogui
 
 class Window():
     def __init__(self):
@@ -7,10 +8,10 @@ class Window():
         self.win.title("Encrypt Files")
         self.win.geometry("800x800")
     def create_widgets(self):
-        self.encrypt_file = tk.Button(text="Encrypt", font=("Roboto Light", 12), height=4, width=12)
-        self.encrypt_file.place(x=50, y=60)
-        self.decrypt_file = tk.Button(text="Decrypt", font=("Roboto Light", 12), height=4, width=12)
-        self.decrypt_file.place(x=200, y=60)
+        self.encrypt_file_button = tk.Button(text="Encrypt", font=("Roboto Light", 12), height=4, width=12)
+        self.encrypt_file_button.place(x=50, y=60)
+        self.decrypt_file_button = tk.Button(text="Decrypt", font=("Roboto Light", 12), height=4, width=12)
+        self.decrypt_file_button.place(x=200, y=60)
         self.encrypt_decrypt_text_label = tk.Label(text="Enter text to encrypt/decrypt:", font=("Roboto Light", 18))
         self.encrypt_decrypt_text_label.place(x=320, y=160)
         self.text_box = tk.Text(height=24, width=50, font=("Roboto Light", 12))
@@ -20,7 +21,10 @@ class Window():
         self.decrypt_text = tk.Button(text="Decrypt", font=("Roboto Light", 12), height=4, width=12)
         self.decrypt_text.place(x=420, y=680)
     def get_file(self, title):
-        self.file = filedialog.askopenfilename(title=title, initaldir="/", multiple=False)
+        self.file = filedialog.askopenfilename(title=title, initialdir="/", multiple=False)
+    def get_password(self, title, text):
+        result = pyautogui.password(title=title, text=text)
+        return result
     def run(self):
         self.win.mainloop()
 
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from Window.window import Window
-import pyautogui
 import base64
 import os
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
 
 class Main(Window):
     def __init__(self):
@@ -13,10 +13,10 @@ class Main(Window):
         self.debug = True
     def run_win(self):
         self.create_widgets()
-        self.connect()
+        self.connect_buttons_commands()
         self.win.mainloop()
     def create_key(self):
-        password_provided = pyautogui.password(title="enter Secret Key", text="Enter a secret key to encrypt the file (Save it to decrypt it later)")
+        password_provided = self.get_password(title="enter Secret Key", text="Enter a secret key to encrypt the file (Save it to decrypt it later)")
         password = password_provided.encode()
         kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
@@ -26,10 +26,18 @@ class Main(Window):
             backend=default_backend()
         )
         self.key = base64.urlsafe_b64encode(kdf.derive(password))
-        print(self.key)
-    def connect(self):
+    def encrypt_file(self):
+        self.create_key()
+        self.get_file("Select a File to Encrypt")
+        with open(self.file, "rb") as file:
+            data = file.read()
+        fernet = Fernet(self.key)
+        encrypted_data = fernet.encrypt(data)
+        print(encrypted_data)
+            
+    def connect_buttons_commands(self):
         if self.debug:
-            self.encrypt_file.config(command=self.create_key)
+            self.encrypt_file_button.config(command=self.encrypt_file)
 
 
 
